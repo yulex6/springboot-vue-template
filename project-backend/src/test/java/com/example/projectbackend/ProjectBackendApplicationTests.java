e3f0f32f1fa0a90b2bb2d6a2a45ed4929e132040
yzl <874525893@qq.com>
2023-05-29 21:11:16 +0800

    新建项目

---
+++ project-backend/src/test/java/com/example/projectbackend/ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
